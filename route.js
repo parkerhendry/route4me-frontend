e226b4cb2b3126fd43fb54563dc50c18231aa1a8
Parker Hendry <parker@traxxisgps.com>
2025-07-31 10:50:08 -0400

    Driver edit

--- route.js
+++ route.js
@@ -664,7 +664,7 @@ function renderDriverList() {
                     </div>
                     <div class="col-md-4 text-end">
                         <button class="btn btn-outline-secondary btn-sm" onclick="showEditDriverForm('${driver.member_email}')">
-                            <i class="fas fa-edit me-1"></i>Edit!
+                            <i class="fas fa-edit me-1"></i>Edit
                         </button>
                     </div>
                 </div>
@@ -2870,7 +2870,7 @@ function showEditDriverResults(data) {
     cancelEditDriver();
     
     // Re-render the driver list to reflect the updated information
-    renderDriverList();
+    validateUser();
 }
 
 /**
